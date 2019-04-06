2e8b21f73892a08b3b64cf73bee6ca0550b91968
jaipavan <jaipavan@verzon.com>
2019-04-06 20:52:41 +0000

    added pavan1

--- src/main/java/com/trucks/com/App.java
+++ src/main/java/com/trucks/com/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! pavan" );
+        System.out.println( "Hello World! pavan1" );
     }
 }
